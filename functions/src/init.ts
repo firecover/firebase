795dbb74d27115d3ad2c91dfbc8c143f00f163e7
Vajahath Ahmed <vajuoff.1@gmail.com>
2023-07-27 11:06:59 +0530

    removes application default init

--- functions/src/init.ts
+++ functions/src/init.ts
@@ -1,10 +1,9 @@
-import { initializeApp, applicationDefault } from "firebase-admin/app";
+import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
 initializeApp({
   storageBucket: "firecover-dev.appspot.com",
-  credential: applicationDefault(),
 });
 
 export const bucket = getStorage().bucket();
