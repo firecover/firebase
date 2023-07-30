e03c6df714fcb313fe49b638c18713f48b5b390e
Vajahath Ahmed <vajuoff.1@gmail.com>
2023-07-31 02:36:34 +0530

    exposing processing setup as a function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,3 +2,4 @@ import "./init";
 
 export { getSingedUploadURL } from "./get-signed-upload-url";
 export { onCoverageUpload } from "./on-coverage-upload";
+export { onAggregatedCoverageSummaryCreated } from "./on-aggregated-covergae-summary-created";
